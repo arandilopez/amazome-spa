73f2e8ebad09516c56c745d17b70f870a450ea2b
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-17 08:38:18 -0600

    home with products

--- src/js/home/controllers/HomeController.js
+++ src/js/home/controllers/HomeController.js
@@ -1,7 +1,18 @@
 // class HomeController
-HomeController.$inject = ['$scope']
+HomeController.$inject = ['$scope', 'WEB_SERVICE', '$http']
 
-function HomeController ($scope) {
+function HomeController ($scope, WEB_SERVICE, $http) {
+  $scope.products = []
+
+  _fetchProducts()
+
+  function _fetchProducts(limit = 21, filter = '') {
+    let url = encodeURI(`${WEB_SERVICE}/products?limit=${limit}&filter=${filter}`)
+    console.log(url);
+    $http.get(url).then(function (response) {
+      $scope.products = response.data
+    })
+  }
 }
 
 module.exports = HomeController
